d5c2de0b14727339a92e88a311be0da7cdd2167f
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-04-26 14:18:28 -0300

    Injetando views

--- app/src/main/java/infoglobo/com/br/facanamanteiga/MainActivity.java
+++ app/src/main/java/infoglobo/com/br/facanamanteiga/MainActivity.java
@@ -2,12 +2,34 @@ package infoglobo.com.br.facanamanteiga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        myTextView.setText("Alo mundo, butter knife");
+    }
+
+    @OnClick(R.id.tryMeButton)
+    public void tryMe(View view){
+        Timber.d("Ouch button clicked");
+        Toast.makeText(this, "Ouch !!!", Toast.LENGTH_LONG).show();
+
     }
 }
